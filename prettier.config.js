e908de284a2e093300b1b1636e0ef45d6555589a
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-10-29 12:19:12 +0100

    Add ESLint and Prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  semi: true,
+  printWidth: 100,
+  tabWidth: 1,
+  singleAttributePerLine: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  useTabs: true,
+  vueIndentScriptAndStyle: false,
+};
