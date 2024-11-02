63d23e2361651d03a76d6407cdf6aa2722d57b18
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-11-02 16:03:07 +0100

    website / Clean actors cards

--- projects/website/src/views/home/ViewHomeActors.vue
+++ projects/website/src/views/home/ViewHomeActors.vue
@@ -20,18 +20,10 @@
       <LychenCard
         v-for="(actor, index) in actors"
         :key="index"
-        class="items-stretch justify-end"
+        class="flex flex-col items-stretch justify-start p-4"
       >
-        <Routerlink :to="{ name: RouteViewPrice.name }">
-          <div class="flex flex-col items-start gap-2 rounded-lg bg-center bg-no-repeat p-4 pb-0">
-            <img
-              src="https://images.pexels.com/photos/2280568/pexels-photo-2280568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              class="rounded-lg"
-            />
-            <LychenTitle variant="h6">{{ actor.title }}</LychenTitle>
-          </div>
-          <div class="p-4 opacity-70">{{ actor.text }}</div>
-        </Routerlink>
+        <LychenTitle variant="h6">{{ actor.title }}</LychenTitle>
+        <div class="opacity-70">{{ actor.text }}</div>
       </LychenCard>
     </div>
   </LychenContainer>
@@ -40,9 +32,7 @@
 <script setup lang="ts">
 import { defineAsyncComponent } from 'vue';
 
-import { RouteViewPrice } from '../price';
 import { actors } from '.';
-import { useTranslations } from './i18n';
 
 const LychenTitle = defineAsyncComponent(
   () => import('@lychen/ui-components/title/LychenTitle.vue'),
@@ -54,6 +44,4 @@ const LychenCard = defineAsyncComponent(() => import('@lychen/ui-components/card
 const LychenParagraph = defineAsyncComponent(
   () => import('@lychen/ui-components/paragraph/LychenParagraph.vue'),
 );
-
-const { t } = useTranslations();
 </script>
