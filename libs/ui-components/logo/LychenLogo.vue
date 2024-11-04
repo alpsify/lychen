aec59ae51a316fc6d26139d70f3319b1ba00200e
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-11-04 17:02:15 +0100

    Add missing peer dependencies

--- libs/ui-components/logo/LychenLogo.vue
+++ libs/ui-components/logo/LychenLogo.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-row gap-2 items-center">
+  <div class="flex flex-row items-center gap-2">
     <img
       src="/logos/lychen/logo-lychen.png"
       class="h-10"
     />
-    <div class="flex flex-col gap-0 antialiased items-start justify-center">
-      <p class="text-xl font-bold font-lexend leading-5">Lychen</p>
-      <small class="text-sm text-surface-container-on/60">by Alpsify</small>
+    <div class="flex flex-col items-start justify-center gap-0 antialiased">
+      <p class="font-lexend text-xl font-bold leading-5">Lychen</p>
+      <small class="text-surface-container-on/60 text-sm">by Alpsify</small>
     </div>
   </div>
 </template>
