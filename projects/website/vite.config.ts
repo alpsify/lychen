7dbdad0348389912b7fb682bbe5a7d57dde35b0d
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-11-05 08:26:41 +0100

    website / clean Vite config

--- projects/website/vite.config.ts
+++ projects/website/vite.config.ts
@@ -6,7 +6,6 @@ import tailwind from 'tailwindcss';
 import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     https: {},
@@ -35,11 +34,6 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './src/assets'),
       '@views': path.resolve(__dirname, './src/views'),
       '@layouts': path.resolve(__dirname, './src/layouts'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      //external: [/logos/, /fonts/],
     },
   },
 });
