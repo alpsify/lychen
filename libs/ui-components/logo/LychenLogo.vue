ba314e122be4a65e0301f06eb47c766466dab1e8
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-11-19 14:31:40 +0100

    website / add alt property to logo component

--- libs/ui-components/logo/LychenLogo.vue
+++ libs/ui-components/logo/LychenLogo.vue
@@ -3,11 +3,11 @@
     <img
       src="/logos/lychen/logo-lychen.svg"
       class="h-4/5 w-auto self-center"
+      alt="Lychen logo"
     />
 
     <div class="flex flex-col items-start justify-center gap-0 antialiased">
       <p class="font-lexend text-xl font-bold leading-5 tracking-wide antialiased">lychen</p>
-      <!--<small class="text-surface-container-on/60 text-sm">by Alpsify</small>-->
     </div>
   </div>
 </template>
