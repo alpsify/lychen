8a52b10d583df320bb456020c9e4f8670c844ece
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-12-22 11:31:37 +0100

    Fix crofunding link

--- projects/website/src/pages/home/PageHomeCrowfunding.vue
+++ projects/website/src/pages/home/PageHomeCrowfunding.vue
@@ -7,41 +7,44 @@
       icon="bullhorn"
       class="fa-2xl"
     />
-    <LychenGlowBorder
-      class="grow hover:shadow-md rounded-lg cursor-pointer"
-      :color="[
-        'rgb(var(--color-primary))',
-        'rgb(var(--color-secondary))',
-        'rgb(var(--color-tertiary))',
-      ]"
+    <a
+      href="https://fr.ulule.com/lychen/coming-soon/"
+      target="_blank"
     >
-      <a
-        href="https://fr.ulule.com/lychen/coming-soon/"
-        target="_blank"
-        class="p-6 rounded-lg bg-surface-container flex flex-col md:grid md:grid-cols-[1fr_30%_20%] gap-4"
+      <LychenGlowBorder
+        class="grow hover:shadow-md rounded-lg cursor-pointer"
+        :color="[
+          'rgb(var(--color-primary))',
+          'rgb(var(--color-secondary))',
+          'rgb(var(--color-tertiary))',
+        ]"
       >
-        <div class="flex flex-col items-center md:items-start">
-          <LychenTextGenerateEffect
-            :words="t('crowfunding.title')"
-            class="text-xl font-bold opacity-90"
-          />
-          <LychenParagraph>{{ t('crowfunding.description') }}</LychenParagraph>
+        <div
+          class="p-6 rounded-lg bg-surface-container flex flex-col md:grid md:grid-cols-[1fr_30%_20%] gap-4"
+        >
+          <div class="flex flex-col items-center md:items-start">
+            <LychenTextGenerateEffect
+              :words="t('crowfunding.title')"
+              class="text-xl font-bold opacity-90"
+            />
+            <LychenParagraph>{{ t('crowfunding.description') }}</LychenParagraph>
+          </div>
+          <div class="flex flex-row items-center justify-center gap-4">
+            <LychenLogo class="h-14" />
+            <p class="opacity-50">X</p>
+            <img
+              :src="isDark ? imageUrl : imageUrlLight"
+              class="h-14 object-cover"
+              alt="Ulule Logo"
+            />
+          </div>
+          <div class="flex flex-col gap-2 justify-center items-end">
+            <p>Le 01/02/2025</p>
+            <p class="text-sm text-right font-bold">{{ t('crowfunding.preregister') }}</p>
+          </div>
         </div>
-        <div class="flex flex-row items-center justify-center gap-4">
-          <LychenLogo class="h-14" />
-          <p class="opacity-50">X</p>
-          <img
-            :src="isDark ? imageUrl : imageUrlLight"
-            class="h-14 object-cover"
-            alt="Ulule Logo"
-          />
-        </div>
-        <div class="flex flex-col gap-2 justify-center items-end">
-          <p>Le 01/02/2025</p>
-          <p class="text-sm text-right font-bold">{{ t('crowfunding.preregister') }}</p>
-        </div>
-      </a>
-    </LychenGlowBorder>
+      </LychenGlowBorder>
+    </a>
   </LychenContainer>
 </template>
 
