37d57240aaee98fb70e9e3a345631d5becd17e29
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-12-29 17:37:36 +0100

    vara-website / Creation

---
+++ projects/vara/website/src/layouts/i18n/fr-FR.ts
@@ -0,0 +1,14 @@
+export default {
+  navigation: {
+    home: {
+      title: 'Accueil',
+    },
+    features: {
+      title: 'Fonctionnalités',
+    },
+  },
+  footer: {
+    seo_paragraph:
+      'Vara accompagne citoyens et associations pour préserver la biodiversité. Participez à des recensements, accédez à des données locales et découvrez des actions pour votre territoire.',
+  },
+};
---
+++ projects/vara/website/src/pages/home/i18n/fr-FR.ts
@@ -0,0 +1,19 @@
+export default {
+  meta: {
+    title: 'Protégez la biodiversité avec Vara',
+    description:
+      'Vara accompagne citoyens et associations pour préserver la biodiversité. Participez à des recensements, accédez à des données locales et découvrez des actions pour votre territoire.',
+    og_title: '@:page_home.meta.title',
+    og_description: '@:page_home.meta.description',
+  },
+  hero: {
+    title: 'Agissez pour la biodiversité avec Vara',
+    description:
+      'Vara connecte citoyens et associations pour protéger la biodiversité. Recensez les espèces, partagez des savoirs et agissez pour la nature.',
+    button: {
+      features: {
+        label: 'Voir les fonctionnalités',
+      },
+    },
+  },
+};
