3cd495c4fefb7e932d8544be609275e918f06bce
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-12-29 14:15:59 +0100

    nginx-util-configs / Generalize configs and task related

--- libs/website/ui-components/sections/application-features/WebsiteSectionApplicationFeatures.vue
+++ libs/website/ui-components/sections/application-features/WebsiteSectionApplicationFeatures.vue
@@ -9,12 +9,18 @@
 </template>
 
 <script setup lang="ts">
-import ApplicationsGridFeatures from '@lychen/applications-ui-components/grids/ApplicationsGridFeatures.vue';
-import { OrganizedFeaturesByGroup } from '@lychen/applications-util-model/OrganizedFeaturesByGroup';
-import LychenTitle from '@lychen/ui-components/title/LychenTitle.vue';
 import { defineAsyncComponent } from 'vue';
+import { OrganizedFeaturesByGroup } from '@lychen/applications-util-model/OrganizedFeaturesByGroup';
 import { TRANSLATION_KEY, messages } from './i18n';
 import { useI18nExtended } from '@lychen/vue-i18n-util-composables/useI18nExtended';
+
+const ApplicationsGridFeatures = defineAsyncComponent(
+  () => import('@lychen/applications-ui-components/grids/ApplicationsGridFeatures.vue'),
+);
+
+const LychenTitle = defineAsyncComponent(
+  () => import('@lychen/ui-components/title/LychenTitle.vue'),
+);
 
 const LychenContainer = defineAsyncComponent(
   () => import('@lychen/ui-components/container/LychenContainer.vue'),
