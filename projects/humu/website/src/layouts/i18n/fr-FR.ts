86880e1778947372c858771b5d49975a6fbbf39c
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-12-29 17:30:20 +0100

    humu-website / Creation

---
+++ projects/humu/website/src/layouts/i18n/fr-FR.ts
@@ -0,0 +1,14 @@
+export default {
+  navigation: {
+    home: {
+      title: 'Accueil',
+    },
+    features: {
+      title: 'Fonctionnalités',
+    },
+  },
+  footer: {
+    seo_paragraph:
+      'Humu vous aide à gérer vos biodéchets grâce à des outils pour le compostage. Connectez-vous avec votre communauté pour des pratiques durables et locales.',
+  },
+};
---
+++ projects/humu/website/src/pages/home/i18n/fr-FR.ts
@@ -0,0 +1,19 @@
+export default {
+  meta: {
+    title: 'Réduisez vos déchets et compostez avec Humu',
+    description:
+      'Humu vous aide à gérer vos biodéchets grâce à des outils pour le compostage. Connectez-vous avec votre communauté pour des pratiques durables et locales.',
+    og_title: '@:page_home.meta.title',
+    og_description: '@:page_home.meta.description',
+  },
+  hero: {
+    title: 'Transformez vos biodéchets en richesse avec Humu',
+    description:
+      'Humu facilite la gestion des biodéchets à travers des outils collaboratifs. Participez à des initiatives locales de compostage et suivez l’impact de vos contributions.',
+    button: {
+      features: {
+        label: 'Voir les fonctionnalités',
+      },
+    },
+  },
+};
