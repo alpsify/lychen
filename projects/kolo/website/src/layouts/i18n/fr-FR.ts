9530e68e8308cf890724b01d80e550c4a429d232
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-12-29 17:53:22 +0100

    kolo-website / Creation

---
+++ projects/kolo/website/src/layouts/i18n/fr-FR.ts
@@ -0,0 +1,14 @@
+export default {
+  navigation: {
+    home: {
+      title: 'Accueil',
+    },
+    features: {
+      title: 'Fonctionnalités',
+    },
+  },
+  footer: {
+    seo_paragraph:
+      'Kolo connecte citoyens et producteurs grâce à une plateforme interactive. Trouvez des magasins locaux, participez à des initiatives solidaires, et gérez vos produits en toute simplicité.',
+  },
+};
---
+++ projects/kolo/website/src/pages/home/i18n/fr-FR.ts
@@ -0,0 +1,19 @@
+export default {
+  meta: {
+    title: 'Découvrez et soutenez les magasins de producteurs avec Kolo',
+    description:
+      'Kolo connecte citoyens et producteurs grâce à une plateforme interactive. Trouvez des magasins locaux, participez à des initiatives solidaires, et gérez vos produits en toute simplicité.',
+    og_title: '@:page_home.meta.title',
+    og_description: '@:page_home.meta.description',
+  },
+  hero: {
+    title: "Soutenez l'économie locale avec Kolo",
+    description:
+      "Kolo met en relation citoyens, producteurs et communautés pour renforcer l'économie locale. Localisez les magasins de producteurs, participez à des initiatives solidaires et découvrez des produits du terroir.",
+    button: {
+      features: {
+        label: 'Voir les fonctionnalités',
+      },
+    },
+  },
+};
