e60398c3b4e273372001084e4506f9bfcd4932a0
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-03-06 20:01:57 +0100

    ADd image to Herbarium

--- projects/tera/app/src/pages/herbarium/PageHerbarium.vue
+++ projects/tera/app/src/pages/herbarium/PageHerbarium.vue
@@ -1,13 +1,20 @@
 <template>
   <SectionWithTitle :title="t('title')">
-    <div
-      class="bg-gradient-to-tr from-tertiary-container to-tertiary-container/70 p-8 rounded-xl flex flex-col gap-4"
+    <DivWithBackgroundImg
+      :background-image="bannerImg"
+      overlay
+      overlay-class="bg-gradient-to-tr from-tertiary-container to-tertiary-container/60"
+      class="p-8 lg:py-20 rounded-xl flex flex-col gap-4 text-on-tertiary-container"
     >
-      <BaseHeading variant="h2">{{ t('banner.title') }}</BaseHeading>
-      <p class="lg:w-2/3">
+      <BaseHeading
+        variant="h2"
+        class="z-10 text-on-tertiary-container"
+        >{{ t('banner.title') }}</BaseHeading
+      >
+      <p class="lg:w-2/3 z-10">
         {{ t('banner.description') }}
       </p>
-    </div>
+    </DivWithBackgroundImg>
     <div>
       <BaseHeading variant="h2">
         {{ t('global.title') }}
@@ -24,10 +31,12 @@
 </template>
 
 <script lang="ts" setup>
+import bannerImg from './assets/banner.webp';
 import SectionWithTitle from '@lychen/vue-ui-components-app/section-with-title/SectionWithTitle.vue';
 import { messages, TRANSLATION_KEY } from './i18n';
 import { useI18nExtended } from '@lychen/vue-i18n-util-composables/useI18nExtended';
 import { BaseHeading } from '@lychen/vue-ui-components-app/base-heading';
+import { DivWithBackgroundImg } from '@lychen/vue-ui-components-extra/div-with-background-img';
 
 const { t } = useI18nExtended({ messages, rootKey: TRANSLATION_KEY, prefixed: true });
 </script>
