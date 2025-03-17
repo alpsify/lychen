13d9909e93f4a70ae698b443effe0aed360cd8e0
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-03-17 08:53:03 +0100

    ADd Combobox and TagsInput base components

---
+++ libs/vue/ui-components-core/tags-input/TagsInputItemText.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { cn } from '@lychen/typescript-util-tailwind/Cn';
+import { TagsInputItemText, type TagsInputItemTextProps, useForwardProps } from 'reka-ui';
+import { computed, type HTMLAttributes } from 'vue';
+
+const props = defineProps<TagsInputItemTextProps & { class?: HTMLAttributes['class'] }>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <TagsInputItemText
+    v-bind="forwardedProps"
+    :class="cn('py-1 px-2 text-sm rounded bg-transparent', props.class)"
+  />
+</template>
