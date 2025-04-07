e0cef7803078a4f42834df6d6a8af3ea3ce0f8d1
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-04-07 18:52:02 +0200

    Add calendar component

---
+++ libs/vue/ui-components-core/calendar/CalendarNextButton.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { cn } from '@lychen/typescript-util-tailwind/Cn';
+import { VARIANT_VALUES } from '../button';
+import { CalendarNext, type CalendarNextProps, useForwardProps } from 'reka-ui';
+import { computed, type HTMLAttributes } from 'vue';
+import Icon from '../icon/Icon.vue';
+import { faChevronRight } from '@fortawesome/pro-light-svg-icons/faChevronRight';
+
+const props = defineProps<CalendarNextProps & { class?: HTMLAttributes['class'] }>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <CalendarNext
+    data-slot="calendar-next-button"
+    :class="
+      cn(
+        VARIANT_VALUES['container-high'],
+        'absolute right-1',
+        'size-7 bg-transparent p-0 opacity-50 hover:opacity-100',
+        props.class,
+      )
+    "
+    v-bind="forwardedProps"
+  >
+    <slot>
+      <Icon
+        :icon="faChevronRight"
+        class="size-4"
+      />
+    </slot>
+  </CalendarNext>
+</template>
---
+++ libs/vue/ui-components-core/calendar/CalendarPrevButton.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { cn } from '@lychen/typescript-util-tailwind/Cn';
+import { VARIANT_VALUES } from '../button';
+import { CalendarPrev, type CalendarPrevProps, useForwardProps } from 'reka-ui';
+import { computed, type HTMLAttributes } from 'vue';
+import Icon from '../icon/Icon.vue';
+import { faChevronLeft } from '@fortawesome/pro-light-svg-icons/faChevronLeft';
+
+const props = defineProps<CalendarPrevProps & { class?: HTMLAttributes['class'] }>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <CalendarPrev
+    data-slot="calendar-prev-button"
+    :class="
+      cn(
+        VARIANT_VALUES['container-high'],
+        'absolute left-1',
+        'size-7 bg-transparent p-0 opacity-50 hover:opacity-100',
+        props.class,
+      )
+    "
+    v-bind="forwardedProps"
+  >
+    <slot>
+      <Icon
+        :icon="faChevronLeft"
+        class="size-4"
+      />
+    </slot>
+  </CalendarPrev>
+</template>
