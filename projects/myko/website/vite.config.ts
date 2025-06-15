8f3ae20b88624af4bf3f94f0a274630d0e8033c1
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-06-15 11:28:20 +0200

    Add devtools as dependencies

--- projects/myko/website/vite.config.ts
+++ projects/myko/website/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import mkcert from 'vite-plugin-mkcert';
 import generateSitemap from 'vite-ssg-sitemap';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 const config: UserConfig = {
   server: {
@@ -13,6 +14,7 @@ const config: UserConfig = {
   },
 
   plugins: [
+    vueDevTools(),
     tailwindcss(),
     mkcert({
       hosts: ['myko.lychen.local'],
