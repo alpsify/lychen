dcc778576dc00019b88301690a9d062fcead4794
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-06-15 14:33:02 +0200

    tera-api / Update dependencies (#40)
    * Update dependencies
    
    * Upgrade to symfony 7.3
    
    * Upgrade api/platform to 4.1
    
    * Upgrade Zenstruck
    
    * Update phpunit  + fix deprecation from nullable parameters
    
    * Add devtools as dependencies
    
    * Update dependecies in php libs
    
    * Update main composer
    
    * Update tools
    
    * design / configure for deployment
    
    * Try to counter not yet supported PHP8.4

--- projects/tera/app/vite.config.ts
+++ projects/tera/app/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import mkcert from 'vite-plugin-mkcert';
 import { VitePWA } from 'vite-plugin-pwa';
 import tailwindcss from '@tailwindcss/vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 const config: UserConfig = {
   server: {
@@ -12,6 +13,7 @@ const config: UserConfig = {
     port: 8080,
   },
   plugins: [
+    vueDevTools(),
     tailwindcss(),
     mkcert({
       hosts: ['app.tera.lychen.local'],
