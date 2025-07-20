e83eaf51864f031e00707cd9f6fb5e048bea7684
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-07-20 17:02:32 +0200

    Fix tera website i18n

--- projects/tera/website/src/App.vue
+++ projects/tera/website/src/App.vue
@@ -7,7 +7,7 @@ import { useI18nExtended } from '@lychen/vue-i18n/composables/useI18nExtended';
 import { usePreferredColorScheme } from '@lychen/vue-color-scheme/composables/usePreferredColorScheme';
 import { defineOrganization, defineWebPage, defineWebSite, useSchemaOrg } from '@unhead/schema-org';
 import { useHead } from '@unhead/vue';
-import { TRANSLATION_KEY, messages } from '@lychen/vue-i18n/tera';
+import { TRANSLATION_KEY, messages } from '@lychen/vue-tera/i18n';
 
 const { t } = useI18nExtended({ messages, rootKey: TRANSLATION_KEY, prefixed: true });
 
